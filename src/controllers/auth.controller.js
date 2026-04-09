28590fb3c30d4f1ed3c9e3ababca2e896f5454e3
piyapathongron-art <piyapathongron@gmail.com>
2026-04-09 14:01:50 +0700

    refactor: sanitize user registration response to exclude sensitive data and improve code formatting

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,31 +1,33 @@
-import createHttpError from "http-errors"
-import { registerSchema } from "../validations/schema.js"
-import { createNewUser, findUseerByEmail } from "../services/auth.service.js"
+import createHttpError from "http-errors";
+import { registerSchema } from "../validations/schema.js";
+import { createNewUser, findUseerByEmail } from "../services/auth.service.js";
 
-export async function authRegisterController(req,res,next){
-      const data = await registerSchema.parseAsync(req.body)
-
-      const foundUser = await findUseerByEmail(data.email)
-
-       if(foundUser) {
-    return next(createHttpError[409]('This user already register'))
+export async function authRegisterController(req, res, next) {
+  const data = await registerSchema.parseAsync(req.body);
+  const foundUser = await findUseerByEmail(data.email);
+  if (foundUser) {
+    return next(createHttpError[409]("This user already register"));
   }
+  const newUser = await createNewUser(data);
 
-    const newUser = await createNewUser(data)
-
-      res.json({
-        message : 'Register Successful',
-        user : data
-      })
+  res.json({
+    message: "Register Successful",
+    user: {
+      id: newUser.id,
+      email: newUser.email,
+      name: newUser.name,
+      role: newUser.role,
+    },
+  });
 }
 
-export function authLoginController(req,res){
-       res.json({
-        msg : "login path",
-        body : req.body
-    })
+export function authLoginController(req, res) {
+  res.json({
+    msg: "login path",
+    body: req.body,
+  });
 }
 
-export function authGetmeController(req,res){
-    res.json("get me path")
+export function authGetmeController(req, res) {
+  res.json("get me path");
 }
